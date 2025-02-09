1b8a4b45fd63a3a2bb389408f45c11ac85097ffd
Ramo0o <omar.moelhawary@gmail.com>
2025-02-09 23:29:34 +0200

    initializing the repo

--- injecting_script.js
+++ injecting_script.js
@@ -1,13 +1,21 @@
+
 setTimeout(main,2000)
 function main(){
     //get the quiz number retrieve the corresponding quiz from the storage 
-    getQuizNo().then(data=>{
-        console.log(data)
-        injectClassName(data)
-        })
+    getQuizData().then(data => {
+        if (data && Array.isArray(data) && data.length > 0) {
+            console.log(data);
+            injectClassName(data);
+        } else {
+            console.warn("No wrong questions found or invalid data.");
+        }
+    });
 }
 
-async function getQuizNo(){
+
+
+
+async function getQuizData(){
     const result = await chrome.storage.local.get(["wrongQuestions"]);
     if (result["wrongQuestions"].length === 0){
         alert("عاش يا وحش")
@@ -15,16 +23,22 @@ async function getQuizNo(){
     else{
         return result["wrongQuestions"];
     }
-
 }
 
+
+
+
+
 function injectClassName(wrongQuestions){
+    //get the buttons of the question numbers
     const elements = document.getElementsByClassName("text-15 lh-1");
+    //intialize an index for iterating over the wrongQuestions array instead of an nested for loops to save time and power 
     let questionsIndex = 0
+    //iterate over the buttons of the questions and check wether or not they match with the questionindex and if so inject the class into that element
+
     for (i in elements){
         console.log(i,Number(wrongQuestions[questionsIndex]-1))
         if (i == Number(wrongQuestions[questionsIndex]-1)){
-            console.log(i)
             elements[i].parentElement.classList.add("-red-3")
             questionsIndex++
         }
